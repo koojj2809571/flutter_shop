b3c470d5f5b632feec511547b31af682c9cf8b25
Opipi <koojj2809571@iCloud.com>
2019-02-27 22:35:48 +0800

    dio练习

---
+++ lib/config/http_headers.dart
@@ -0,0 +1,12 @@
+const httpHeaders = {
+'Accept': 'application/json, text/plain, */*',
+'Accept-Encoding': 'gzip, deflate, br',
+'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7',
+'Connection': 'keep-alive',
+'Content-Type': 'application/json',
+'Cookie': '_ga=GA1.2.777539104.1551276241; _gid=GA1.2.1795624384.1551276241; _gat=1; GCID=29891c7-b28e8c5-f49fa6f-dd0d57d-cd; SERVERID=1fa1f330efedec1559b3abbcb6e30f50|1551276251|1551276239; Hm_lvt_022f847c4e3acd44d4a2481d9187f1e6=1551276240,1551276252; Hm_lpvt_022f847c4e3acd44d4a2481d9187f1e6=1551276252',
+'Host': 'time.geekbang.org',
+'Origin': 'https://time.geekbang.org',
+'Referer': 'https://time.geekbang.org/',
+'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
+};
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import '../config/http_headers.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatefulWidget {
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String showText = '还没有请求导数据';
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('商城首页'),
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('访问远程数据'),
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: _choiceAction,
+                  child: Text('请求数据'),
+                ),
+                Text(showText),
+              ],
+            ),
+          ),
+        ),
       ),
     );
+  }
+
+  void _choiceAction() {
+    print('开始向极客时间请求数据..................');
+    getHttpRequest().then((val) {
+      print('收到返回数据');
+      setState(() {
+        showText = val['data'].toString();
+      });
+    });
+  }
+
+  Future getHttpRequest() async {
+    try {
+      Response response;
+      Dio dio = new Dio();
+      dio.options.headers = httpHeaders;
+      response = await dio.get(
+        "https://time.geekbang.org/serv/v1/column/newAll",
+      );
+      return response.data;
+    } catch (e) {
+      return print(e);
+    }
   }
 }
