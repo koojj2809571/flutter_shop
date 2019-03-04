effe742df3be6119f769ad2fa8955ad193946f1e
Opipi <koojj2809571@iCloud.com>
2019-03-04 23:20:45 +0800

    15完成

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import '../service/service_method.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'dart:convert';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
@@ -26,15 +27,20 @@ class _HomePageState extends State<HomePage> {
         appBar: AppBar(
           title: Text('百姓生活+'),
         ),
-        body: SingleChildScrollView(
-          child: FutureBuilder(
-            builder: (context, snapShot) {
-              if (snapShot.hasData) {
-                var data = jsonDecode(snapShot.data.toString());
-                List<Map> swiper = (data['data']['slides'] as List).cast();
-                List<Map> navigatorList = (data['data']['category'] as List).cast();
-                String adPicture = (data['data']['advertesPicture']['PICTURE_ADDRESS']);
-                return Column(
+        body: FutureBuilder(
+          builder: (context, snapShot) {
+            if (snapShot.hasData) {
+              var data = jsonDecode(snapShot.data.toString());
+              List<Map> swiper = (data['data']['slides'] as List).cast();
+              List<Map> navigatorList =
+                  (data['data']['category'] as List).cast();
+              String adPicture =
+                  (data['data']['advertesPicture']['PICTURE_ADDRESS']);
+              String leaderImage = (data['data']['shopInfo']['leaderImage']);
+              String leaderPhone = (data['data']['shopInfo']['leaderPhone']);
+              List<Map> recommendList = (data['data']['recommend'] as List).cast();
+              return SingleChildScrollView(
+                child: Column(
                   children: <Widget>[
                     SwiperDiy(
                       swiperDataList: swiper,
@@ -45,16 +51,23 @@ class _HomePageState extends State<HomePage> {
                     AdBanner(
                       adPicture: adPicture,
                     ),
+                    LeaderPhone(
+                      leaderImage: leaderImage,
+                      leaderPhone: leaderPhone,
+                    ),
+                    Recommend(
+                      recommendList: recommendList,
+                    ),
                   ],
-                );
-              } else {
-                return Center(
-                  child: Text('加载中.....'),
-                );
-              }
-            },
-            future: getHomePageContent(),
-          ),
+                ),
+              );
+            } else {
+              return Center(
+                child: Text('加载中.....'),
+              );
+            }
+          },
+          future: getHomePageContent(),
         ),
       ),
     );
@@ -115,7 +128,7 @@ class TopNavigator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if(navigatorList.length > 10){
+    if (navigatorList.length > 10) {
       navigatorList.removeRange(10, navigatorList.length);
     }
     return Container(
@@ -133,11 +146,10 @@ class TopNavigator extends StatelessWidget {
 }
 
 //广告图片
-class AdBanner extends StatelessWidget{
-
+class AdBanner extends StatelessWidget {
   final String adPicture;
 
-  AdBanner({Key key,this.adPicture}):super(key:key);
+  AdBanner({Key key, this.adPicture}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -145,5 +157,121 @@ class AdBanner extends StatelessWidget{
       child: Image.network(adPicture),
     );
   }
+}
 
+//店长电话模块
+class LeaderPhone extends StatelessWidget {
+  final String leaderImage;
+  final String leaderPhone;
+
+  LeaderPhone({Key key, this.leaderImage, this.leaderPhone}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: InkWell(
+        onTap: _launchURL,
+        child: Image.network(leaderImage),
+      ),
+    );
+  }
+
+  void _launchURL() async {
+    String url = 'tel:' + leaderPhone;
+//    String url = 'http://jspang.com';
+//    String url = 'sms:15987182704';
+    print(url);
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw '异常----->url不能访问';
+    }
+  }
+}
+
+//商品推荐类
+class Recommend extends StatelessWidget {
+  final List recommendList;
+
+  Recommend({Key key, this.recommendList}) : super(key: key);
+
+  Widget _titleWidget() {
+    return Container(
+      alignment: Alignment.centerLeft,
+      padding: EdgeInsets.fromLTRB(10, 2, 0, 5),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(
+            width: 0.5,
+            color: Colors.black12,
+          ),
+        ),
+      ),
+      child: Text(
+        '商品推荐',
+        style: TextStyle(color: Colors.pink),
+      ),
+    );
+  }
+
+  Widget _itemWidget(index) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        height: ScreenUtil().setHeight(330),
+        width: ScreenUtil().setWidth(250),
+        padding: EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            left: BorderSide(
+              width: 0.5,
+              color: Colors.black12,
+            ),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.network(recommendList[index]['image']),
+            Text(
+              '￥${recommendList[index]['mallPrice']}',
+            ),
+            Text(
+              '￥${recommendList[index]['price']}',
+              style: TextStyle(
+                  color: Colors.grey, decoration: TextDecoration.lineThrough),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _recommendList() {
+    return Container(
+      height: ScreenUtil().setHeight(330),
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: recommendList.length,
+        itemBuilder: (context, index) => _itemWidget(index),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(recommendList.length);
+    return Container(
+      height: ScreenUtil().setHeight(380),
+      margin: EdgeInsets.only(top: 10),
+      child: Column(
+        children: <Widget>[
+          _titleWidget(),
+          _recommendList(),
+        ],
+      ),
+    );
+  }
 }
